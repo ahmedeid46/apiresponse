d1ab8a0ced191243c79fecd85c87ab7ed04ffd3f
ahmedeid46 <ahmed.eid.1@hotmail.com>
2022-10-11 15:58:19 +0200

    Add New Methods

--- src/APIResponse.php
+++ src/APIResponse.php
@@ -8,12 +8,53 @@ use http\Client\Response;
 
 class APIResponse
 {
+    use setVariable;
     public function respondNotFound()
     {
-        return response()->json([
-            'name' => 'Abigail',
-            'state' => 'CA',
-        ]);
+        $array =[
+            'status'=>false,
+            'message' => $this->message
+        ];
+        if ($this->data !=null){
+            $array['data'] = $this->data;
+        }
+        return response()->json($array)->setStatusCode(Response::HTTP_NOT_FOUND);
     }
+    public function respondOK()
+    {
+        $array =[
+            'status'=>true,
+            'message' => $this->message
+        ];
+        if ($this->data !=null){
+            $array['data'] = $this->data;
+        }
+        return response()->json($array)->setStatusCode(Response::OK);
+    }
+    public function serverErorr()
+    {
+        $array =[
+            'status'=>false,
+            'message' => 'server error',
+        ];
+        return response()->json($array)->setStatusCode(Response::ServerError);
+    }
+    public function validationFailed(){
+        $array =[
+            'status'=>false,
+        ];
+        if ($this->message != null){
+            $array['message'] = $this->message;
+        }else{
+            $array['message'] = 'Validation Failed';
+        }
+        if ($this->data != null){
+            foreach($this->data as $key=>$value){
+                $array['error']['$key'] = $value[1];
+            }
+        }
+        return response()->json($array)->setStatusCode(Response::BADREQUEST);
+    }
+
 
 }
